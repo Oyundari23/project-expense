cbae5a49b9bacda18bed1af27ccf3e4a1caedeac
Oyundari23 <dariaa0560@gmail.com>
2024-10-17 19:39:20 +0800

    angid

--- service /main.js
+++ service /main.js
@@ -1,22 +1,58 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 4000;
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.get("/articles", (req, res) => {
+  res.json([
+    { id: 1, title: "green" },
+    { id: 2, title: "juice" },
+  ]);
+});
+const categories = [
+  {
+    name: "shopping",
+    age: 10,
+  },
+  {
+    name: "drink",
+    age: 12,
+  },
+];
 
-app.get('/articles', (req, res) => {
-    res.json ([
-      {id:1, title: "hello"},
-      {id:2, title: "world"},
-      ])
-  })
+app.get("/categories/list", (req, res) => {
+  res.json(categories);
+});
+
+app.get("/categories/create", (req, res) => {
+  const { name } = req.query;
+  categories.push({ name: name });
+  res.send("success!");
+});
+
+const cart = [
+  {
+    name: "carl",
+  },
+];
+
+app.get("/listening/list", (req, res) => {
+  res.send(cart);
+});
+
+app.get("/listening/create", (req, res) => {
+  const { name } = req.query;
+  cart.push({ name: name });
+  res.send("success");
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
